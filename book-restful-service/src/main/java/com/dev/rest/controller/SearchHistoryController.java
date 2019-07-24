d93d48c37e6f83d97e71aee716dfce145394e4ac
rchani <rchani@uangel.com>
2019-07-24 22:12:20 +0900

    나의 검색 히스토리 API 추가

--- book-restful-service/src/main/java/com/dev/rest/controller/SearchHistoryController.java
+++ book-restful-service/src/main/java/com/dev/rest/controller/SearchHistoryController.java
@@ -1,18 +1,23 @@
-package com.dev.rest.data.repository;
+package com.dev.rest.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dev.rest.data.SearchHistory;
+import com.dev.rest.message.PagingInfo;
+import com.dev.rest.security.AuthInfo;
 import com.dev.rest.service.SearchHistoryService;
 
 @RestController
@@ -20,16 +25,14 @@ import com.dev.rest.service.SearchHistoryService;
 public class SearchHistoryController {
 	Logger log = LoggerFactory.getLogger(getClass());
 	
-
 	@Autowired
 	SearchHistoryService searchHistoryService;
 	
+	@Secured({ "IS_AUTHENTICATED_FULLY" })
 	@RequestMapping(path="/history", method=RequestMethod.GET)
-	public Page<SearchHistory> getMySearchHistory(
-			@PageableDefault(page = 0, size = 10, sort = "createdDate", direction = Sort.Direction.DESC) Pageable pageable){
-		//FIXME user 넣어주기 !!
-//		searchHistoryService.getSearchHistoryByUser(user, pageable);
-		return null;
+	@ResponseBody
+	public Page<SearchHistory> getMySearchHistory(@RequestBody PagingInfo pagingInfo, AuthInfo authInfo) {
+		Pageable pageable = new PageRequest(pagingInfo.getPage(), pagingInfo.getSize(), Sort.Direction.DESC, "createdDate");
+		return searchHistoryService.getSearchHistoryByUser(authInfo.getUser(), pageable);
 	}
-
 }
